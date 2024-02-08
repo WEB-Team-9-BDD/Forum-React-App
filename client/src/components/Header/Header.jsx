008092b8bdbc5bacb6bc29576a164cfaaf92d085
borislavpachev <borislav.pachev@gmail.com>
2024-02-08 19:36:45 +0200

    header component added

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,3 +1,10 @@
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
 
+    return (
+        <header>
+        <NavLink to="/">Home</NavLink>
+        </header>
+    )
 }
