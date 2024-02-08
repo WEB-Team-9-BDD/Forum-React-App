bad6d79bb6d9d757d4bc953cc302c4fd3e3fae84
borislavpachev <borislav.pachev@gmail.com>
2024-02-09 00:33:10 +0200

    app useauthstate added so user can stay logged in when browser window is refreshed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Home from './views/Home/Home'
-import {AppContext} from './context/AppContext'
+import Header from './components/Header/Header'
+import { AppContext } from './context/AppContext'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './views/Login/Login'
+import CreateAccount from './views/CreateAccount/CreateAccount'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from './config/firebase-config'
+import Loader from './components/Loader/Loader'
 
 function App() {
   const [appState, setAppState] = useState({
     user: null,
     userData: null,
-  })
+  });
+  const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      setAppState({ user, userData: null });
+    }
+  }, [user]);
+
+  // if(loading) {
+  //   setTimeout(() => {
+  //     <Loader/>  
+  //   }, 500);
+  //    return    
+  // }
+
+
+
 
   return (
     <>
       <BrowserRouter>
         <AppContext.Provider value={{ ...appState, setAppState }}>
+          <Header />
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route index element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/create-account' element={<CreateAccount />} />
           </Routes>
         </AppContext.Provider>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
