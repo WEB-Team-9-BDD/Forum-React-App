f1301713a89b9b12cf24ee3a0a06ffc415b84ef4
borislavpachev <borislav.pachev@gmail.com>
2024-02-09 00:31:41 +0200

    header added

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,28 @@
 import { NavLink } from "react-router-dom";
+import "./Header.css"
+import { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+import { logoutUser } from "../../services/auth.service";
 
 export default function Header() {
+    const { user, setAppState } = useContext(AppContext);
+    console.log(user);
+    const logout = async () => {
+        await logoutUser();
+        setAppState({ user: null, userData: null });
+    }
 
     return (
         <header>
-        <NavLink to="/">Home</NavLink>
-        </header>
+            <NavLink to="/">Home</NavLink>
+
+            {user ?
+                (
+                    <button onClick={logout}>Log out</button>
+                ) : (
+                    <NavLink to="/login">Log in</NavLink>
+                )
+            }
+        </header >
     )
 }
