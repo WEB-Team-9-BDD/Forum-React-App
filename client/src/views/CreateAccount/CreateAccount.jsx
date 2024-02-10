550a664ecbc24b26ae4d568187adcea028d84898
borislavpachev <borislav.pachev@gmail.com>
2024-02-10 14:28:56 +0200

    create account with empty names fixed

--- client/src/views/CreateAccount/CreateAccount.jsx
+++ client/src/views/CreateAccount/CreateAccount.jsx
@@ -29,8 +29,9 @@ export default function CreateAccount() {
     const createUser = async () => {
         try {
             const user = await getUserByUsername(form.username);
-            if (!user && user.exists()) {
+            if (user.exists()) {
                 toast.error('Username already exists');
+                return
             }
             const userCredentials = await registerUser(form.email, form.password);
             await createUserUsername(form.username, form.firstName, form.lastName, form.email, userCredentials.user.uid)
@@ -43,51 +44,49 @@ export default function CreateAccount() {
         }
     }
 
-    const handleSubmit = () => {
-        // e.preventDefault();
+    const validateFields = () => {
         const { username, email, password, firstName, lastName } = form;
-        if (firstName <= 3 && firstName >= 32) {
-            toast.error('First name must be between 3 and 32 symbols');
-            return
-        }
-        if (lastName <= 3 && lastName >= 32) {
-            toast.error('Last name must be between 3 and 32 symbols');
-            return
-        }
-        if (email.length === 0) {
-            toast.error('Email cannot be empty');
-            return
-        }
         if (username.length === 0) {
             toast.error('Username cannot be empty');
             return
-        }
-        if (password.length < 6) {
+        } else if (email.length === 0) {
+            toast.error('Email cannot be empty');
+            return
+        } else if (password.length < 6) {
             toast.error('Password must be at least 6 characters');
             return
+        } else if ((firstName <= 4 && firstName >= 32) || !firstName) {
+            toast.error('First name must be between 4 and 32 characters');
+            return
+        } else if ((lastName <= 4 && lastName >= 32) || !lastName) {
+            toast.error('Last name must be between 4 and 32 characters');
+            return
         }
+
+        (async () => createUser())();
     }
+
 
     return (
         <div className="wrapper d-flex align-items-center justify-content-center w-100">
             <div className="create-account">
                 <h1 className="heading mb-3 text-center">Create Account</h1>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={e => e.preventDefault()} >
                     <div className="form-group mb-2 ">
                         <label className="form-label" htmlFor="username">Username: </label>
-                        <input required autoComplete="off" className="form-control"
+                        <input autoComplete="off" className="form-control"
                             type="text" name="username" id="username"
                             value={form.username} onChange={updateForm('username')} />
                     </div>
                     <div className="form-group mb-2 ">
                         <label className="form-label" htmlFor="email">Your e-mail: </label>
-                        <input required autoComplete="off" className="form-control"
+                        <input autoComplete="off" className="form-control"
                             type="email" name="email" id="email" value={form.email}
                             onChange={updateForm('email')} />
                     </div>
                     <div className="form-group mb-2 ">
                         <label className="form-label" htmlFor="password">Password: </label>
-                        <input required autoComplete="off"
+                        <input autoComplete="off"
                             className="form-control" type={showPassword ? 'text' : 'password'}
                             name="password" id="password" value={form.password}
                             onChange={updateForm('password')} />
@@ -98,18 +97,18 @@ export default function CreateAccount() {
                     </div>
                     <div className="form-group mb-2 ">
                         <label className="form-label" htmlFor="first-name">First Name: </label>
-                        <input required autoComplete="off" className="form-control" type="text"
+                        <input autoComplete="off" className="form-control" type="text"
                             name="first-name" id="first-name" value={form.firstName}
                             onChange={updateForm('firstName')} />
                     </div>
                     <div className="form-group mb-2 ">
                         <label className="form-label" htmlFor="last-name">Last Name: </label>
-                        <input required autoComplete="off" className="form-control"
+                        <input autoComplete="off" className="form-control"
                             type="text" name="last-name" id="last-name"
                             value={form.lastName} onChange={updateForm('lastName')} />
                     </div>
                     <button type="submit" className="btn btn-success mt-4 mb-3 w-100"
-                        onClick={createUser}>Create account</button>
+                        onClick={validateFields}>Create account</button>
                 </form>
             </div>
         </div >
