3b188a52d674cf9ef3c3cbdb9bee455455a068e0
borislavpachev <borislav.pachev@gmail.com>
2024-02-10 12:29:11 +0200

    create user error messages fixes

--- client/src/views/CreateAccount/CreateAccount.jsx
+++ client/src/views/CreateAccount/CreateAccount.jsx
@@ -5,6 +5,7 @@ import { createUserUsername, getUserByUsername } from "../../services/users.serv
 import { registerUser } from "../../services/auth.service"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import toast from 'react-hot-toast';
 import './CreateAccount.css'
 
 export default function CreateAccount() {
@@ -26,18 +27,10 @@ export default function CreateAccount() {
     }
 
     const createUser = async () => {
-
-        if (form.firstName <= 3 && form.firstName >= 32) {
-           return alert('First name must be between 3 and 32 symbols');
-        }
-        if (form.lastName <= 3 && form.lastName >= 32) {
-           return alert('Last name must be between 3 and 32 symbols');
-        }
-
         try {
             const user = await getUserByUsername(form.username);
-            if (user.exists()) {
-                return alert('Username already exists');
+            if (!user && user.exists()) {
+                toast.error('Username already exists');
             }
             const userCredentials = await registerUser(form.email, form.password);
             await createUserUsername(form.username, form.firstName, form.lastName, form.email, userCredentials.user.uid)
@@ -46,39 +39,78 @@ export default function CreateAccount() {
             navigate('/');
 
         } catch (error) {
-            alert(error.message);
+            toast.error(error.message);
+        }
+    }
+
+    const handleSubmit = () => {
+        // e.preventDefault();
+        const { username, email, password, firstName, lastName } = form;
+        if (firstName <= 3 && firstName >= 32) {
+            toast.error('First name must be between 3 and 32 symbols');
+            return
+        }
+        if (lastName <= 3 && lastName >= 32) {
+            toast.error('Last name must be between 3 and 32 symbols');
+            return
+        }
+        if (email.length === 0) {
+            toast.error('Email cannot be empty');
+            return
+        }
+        if (username.length === 0) {
+            toast.error('Username cannot be empty');
+            return
+        }
+        if (password.length < 6) {
+            toast.error('Password must be at least 6 characters');
+            return
         }
     }
 
     return (
         <div className="wrapper d-flex align-items-center justify-content-center w-100">
             <div className="create-account">
-                <h1 className="mb-3 text-center">Create Account</h1>
-                <div className="form-group mb-2 ">
-                    <label className="form-label" htmlFor="username">Username: </label>
-                    <input className="form-control" type="text" name="username" id="username" value={form.username} onChange={updateForm('username')} />
-                </div>
-                <div className="form-group mb-2 ">
-                    <label className="form-label" htmlFor="email">Your e-mail: </label>
-                    <input className="form-control" type="email" name="email" id="email" value={form.email} onChange={updateForm('email')} />
-                </div>
-                <div className="form-group mb-2 ">
-                    <label className="form-label" htmlFor="password">Password: </label>
-                    <input className="form-control" type={showPassword ? 'text': 'password'} name="password" id="password" value={form.password} onChange={updateForm('password')} />
-                    <span className="password-span"><FontAwesomeIcon
-                        onClick={() => setShowPassword(!showPassword)}
-                        icon={showPassword ? faEye : faEyeSlash} />
-                    </span>
-                </div>
-                <div className="form-group mb-2 ">
-                    <label className="form-label" htmlFor="first-name">First Name: </label>
-                    <input className="form-control" type="text" name="first-name" id="first-name" value={form.firstName} onChange={updateForm('firstName')} />
-                </div>
-                <div className="form-group mb-2 ">
-                    <label className="form-label" htmlFor="last-name">Last Name: </label>
-                    <input className="form-control" type="text" name="last-name" id="last-name" value={form.lastName} onChange={updateForm('lastName')} />
-                </div>
-                <button className="btn btn-success mt-4 mb-3 w-100 " onClick={createUser}>Create account</button>
+                <h1 className="heading mb-3 text-center">Create Account</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group mb-2 ">
+                        <label className="form-label" htmlFor="username">Username: </label>
+                        <input required autoComplete="off" className="form-control"
+                            type="text" name="username" id="username"
+                            value={form.username} onChange={updateForm('username')} />
+                    </div>
+                    <div className="form-group mb-2 ">
+                        <label className="form-label" htmlFor="email">Your e-mail: </label>
+                        <input required autoComplete="off" className="form-control"
+                            type="email" name="email" id="email" value={form.email}
+                            onChange={updateForm('email')} />
+                    </div>
+                    <div className="form-group mb-2 ">
+                        <label className="form-label" htmlFor="password">Password: </label>
+                        <input required autoComplete="off"
+                            className="form-control" type={showPassword ? 'text' : 'password'}
+                            name="password" id="password" value={form.password}
+                            onChange={updateForm('password')} />
+                        <span className="password-span"><FontAwesomeIcon
+                            onClick={() => setShowPassword(!showPassword)}
+                            icon={showPassword ? faEye : faEyeSlash} />
+                        </span>
+                    </div>
+                    <div className="form-group mb-2 ">
+                        <label className="form-label" htmlFor="first-name">First Name: </label>
+                        <input required autoComplete="off" className="form-control" type="text"
+                            name="first-name" id="first-name" value={form.firstName}
+                            onChange={updateForm('firstName')} />
+                    </div>
+                    <div className="form-group mb-2 ">
+                        <label className="form-label" htmlFor="last-name">Last Name: </label>
+                        <input required autoComplete="off" className="form-control"
+                            type="text" name="last-name" id="last-name"
+                            value={form.lastName} onChange={updateForm('lastName')} />
+                    </div>
+                    <button type="submit" className="btn btn-success mt-4 mb-3 w-100"
+                        onClick={createUser}>Create account</button>
+                </form>
             </div>
         </div >
     )
