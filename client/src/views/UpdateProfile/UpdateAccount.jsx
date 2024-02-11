b6b8fd62d2e56a8588eaec300981b6d61044828b
borislavpachev <borislav.pachev@gmail.com>
2024-02-11 16:28:21 +0200

    component name changed

---
+++ client/src/views/UpdateProfile/UpdateAccount.jsx
@@ -0,0 +1,112 @@
+import { useContext, useState } from "react";
+import { AppContext } from "../../context/AppContext";
+import { useNavigate } from "react-router-dom";
+import toast from 'react-hot-toast';
+import './UpdateProfile.css'
+import { createUserUsername } from "../../services/users.service";
+import { updateEmail, updatePassword } from 'firebase/auth'
+import { logoutUser } from "../../services/auth.service";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+
+export default function UpdateAccount() {
+    const { user, userData, setAppState } = useContext(AppContext);
+    const [showPassword, setShowPassword] = useState({
+        password: false,
+        confirmPassword: false
+    });
+
+    const [form, setForm] = useState({
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        email: userData.email,
+        password: '',
+        confirmPassword: '',
+    });
+    const navigate = useNavigate();
+
+    const updateForm = prop => e => {
+        setForm({ ...form, [prop]: e.target.value })
+    };
+
+    const updateUser = async () => {
+
+        try {
+            if (form.confirmPassword && form.password === form.confirmPassword) {
+                await updatePassword(user, form.confirmPassword);
+                toast.success('Password has been changed successfully!');
+            }
+            form.email !== userData.email ? await updateEmail(user, form.email) : null
+            await createUserUsername(userData.username, form.firstName, form.lastName, form.email, userData.uid);
+            await logoutUser();
+            setAppState({ user: null, userData: null });
+            toast.success('Account updated successfully!');
+            navigate('/');
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+
+    return (
+        <div>
+            <div className="wrapper d-flex align-items-center justify-content-center w-100">
+                <div className="update-account">
+                    <h1 className="heading mb-3 text-center">Update Account</h1>
+                    <form onSubmit={e => e.preventDefault()} >
+                        <div className="form-group mb-2 ">
+                            <label className="form-label" htmlFor="username">Username: </label>
+                            <input readOnly disabled autoComplete="off" className="form-control"
+                                type="text" name="username" id="username"
+                                value={userData.username} />
+                        </div>
+                        <div className="form-group mb-2 ">
+                            <label className="form-label" htmlFor="email">Your e-mail: </label>
+                            <input autoComplete="off" className="form-control"
+                                type="email" name="email" id="email" value={form.email}
+                                onChange={updateForm('email')} />
+                        </div>
+
+                        <div className="form-group mb-2 ">
+                            <label className="form-label" htmlFor="first-name">First Name: </label>
+                            <input autoComplete="off" className="form-control" type="text"
+                                name="first-name" id="first-name" value={form.firstName}
+                                onChange={updateForm('firstName')} />
+                        </div>
+                        <div className="form-group mb-2 ">
+                            <label className="form-label" htmlFor="last-name">Last Name: </label>
+                            <input autoComplete="off" className="form-control"
+                                type="text" name="last-name" id="last-name"
+                                value={form.lastName} onChange={updateForm('lastName')} />
+                        </div>
+                        <hr />
+                        <div className="form-group mb-2 ">
+                            <label className="form-label" htmlFor="password">New password: </label>
+                            <input autoComplete="off"
+                                className="form-control" type={showPassword.password ? 'text' : 'password'}
+                                name="password" id="password" value={form.password}
+                                onChange={updateForm('password')} />
+                            <span className="password-span"><FontAwesomeIcon
+                                onClick={() => setShowPassword({ ...showPassword, password: !showPassword.password })}
+                                icon={showPassword.password ? faEye : faEyeSlash} />
+                            </span>
+                        </div>
+                        <div className="form-group mb-2 ">
+                            <label className="form-label" htmlFor="confirm-password">Confirm password: </label>
+                            <input autoComplete="off"
+                                className="form-control" type={showPassword.confirmPassword ? 'text' : 'password'}
+                                name="confirm-password" id="confirm-password" value={form.confirmPassword}
+                                onChange={updateForm('confirmPassword')} />
+                            <span className="password-span"><FontAwesomeIcon
+                                onClick={() => setShowPassword({ ...showPassword, confirmPassword: !showPassword.confirmPassword })}
+                                icon={showPassword.confirmPassword ? faEye : faEyeSlash} />
+                            </span>
+                        </div>
+                        <button type="submit" className="btn btn-success mt-4 mb-1 w-100"
+                            onClick={updateUser}>Update account</button>
+                    </form>
+                </div>
+            </div >
+        </div>
+    )
+}
