17e09e0a83e73e7b1eec91c07efa4efb58f4691b
DDankov <danko5656@gmail.com>
2024-02-12 00:22:19 +0200

    users.service update

--- client/src/services/users.service.js
+++ client/src/services/users.service.js
@@ -6,6 +6,11 @@ export const getUserByUsername = (username) => {
   return get(ref(db, `users/${username}`));
 };
 
+export const getUserByEmail = (email) => {
+  
+    return get(ref(db, `users'/${email}`));
+};
+
 export const createUserUsername = (username, firstName, lastName,email, uid ) => {
 
   return set(ref(db, `users/${username}`), { username, firstName, lastName, uid, email, createdOn: new Date(), likedPosts: {} })
@@ -14,4 +19,9 @@ export const createUserUsername = (username, firstName, lastName,email, uid ) =>
 export const getUserData = (uid) => {
 
   return get(query(ref(db, 'users'), orderByChild('uid'), equalTo(uid)));
+};
+
+export const makeUserAdmin = (email) => {
+
+  return set(ref(db, `users/${email}/isAdmin`), true);
 };
