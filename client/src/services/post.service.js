43017ded83518490b1fd2307c3d7db2ec26ba130
Dayana Vakareeva <dayana.vakareeva@gmail.com>
2024-02-12 17:26:40 +0200

    posts changes

--- client/src/services/post.service.js
+++ client/src/services/post.service.js
@@ -1,93 +1,103 @@
-import { get, push, update, ref, query, equalTo, orderByChild, runTransaction } from 'firebase/database';
+import {
+  get,
+  push,
+  update,
+  ref,
+  query,  
+  orderByChild,
+  runTransaction,
+} from 'firebase/database';
 import { db } from '../config/firebase-config';
 
 export const addPost = async (author, title, content) => {
-    if (title.length < 16 || title.length > 64) {
-        throw new Error('Title must be between 16 and 64 symbols.');
-      }
-      if (content.length < 32 || content.length > 8192) {
-        throw new Error('Content must be between 32 and 8192 symbols.');
-      }
-    return push (ref(db, 'posts'), {
-        author,
-        title,
-        content,        
-        createdOn: Date.now(),
-        likes: 0,
-        comments: [],
-    })
-}
+  return push(ref(db, 'posts'), {
+    author,
+    title,
+    content,
+    createdOn: Date.now(),
+    likes: 0,
+    dislikes: 0,
+    comments: [],
+  });
+};
 
 export const getAllPosts = async (search, createdBy) => {
-    const snapshot = await get(query(ref(db, 'posts'), orderByChild('createdOn')));
-    if (!snapshot.exists()) {
-        return [];
-    }
+  const snapshot = await get(
+    query(ref(db, 'posts'), orderByChild('createdOn'))
+  );
+  if (!snapshot.exists()) {
+    return [];
+  }
 
-    const posts = Object.keys(snapshot.val()).map(key => ({
-        id: key,
-        ...snapshot.val()[key],
-        createdOn: new Date(snapshot.val()[key].createdOn).toString(),
-        createdBy: snapshot.val()[key].createdBy ? Object.keys(snapshot.val()[key].createdBy) : [],
+  const posts = Object.keys(snapshot.val())
+    .map((key) => ({
+      id: key,
+      ...snapshot.val()[key],
+      createdOn: new Date(snapshot.val()[key].createdOn).toString(),
+      createdBy: snapshot.val()[key].createdBy
+        ? Object.keys(snapshot.val()[key].createdBy)
+        : [],
     }))
-    .filter(p => p.title.toLowerCase().includes(search.toLowerCase()))
-    .filter(p => p.createdBy.includes(createdBy));
+    .filter((p) => p.title.toLowerCase().includes(search.toLowerCase()))
+    .filter((p) => p.createdBy.includes(createdBy));
 
-    console.log(posts);
-    
-    return posts;
-}
+  console.log(posts);
+
+  return posts;
+};
 
 export const getPostById = async (id) => {
-    const snapshot = await get(ref(db, `posts/${id}`));
-    if (snapshot.exists()) {
-        return null;
-    }
+  const snapshot = await get(ref(db, `posts/${id}`));
+  if (snapshot.exists()) {
+    return null;
+  }
 
-    const post = {
-        id, 
-        ...snapshot.val(),
-        createdOn: new Date (snapshot.val().createdOn).toString(),
-        createdBy: snapshot.val().createdBy ? Object.keys(snapshot.val().createdBy) : [],
-    };
+  const post = {
+    id,
+    ...snapshot.val(),
+    createdOn: new Date(snapshot.val().createdOn).toString(),
+    createdBy: snapshot.val().createdBy
+      ? Object.keys(snapshot.val().createdBy)
+      : [],
+  };
 
-    return post;
-}
+  return post;
+};
 
 export const likePost = (handle, postId) => {
-    const postLike = ref(db, `/posts/${postId}`)
-    return runTransaction (postLike, (post) => {
-        if(post) {
-            if(post.likes){
-                post.likes += 1;
-            } else {
-                post.likes = 1
-            }
-        }
-        return post;
-    })
-}
+  const postLike = ref(db, `/posts/${postId}`);
+  return runTransaction(postLike, (post) => {
+    if (post) {
+      if (post.likes) {
+        post.likes += 1;
+      } else {
+        post.likes = 1;
+      }
+    }
+    return post;
+  });
+};
 
 export const dislikePost = (handle, postId) => {
-    const postDislike = ref(db, `/posts/${postId}`);
-    return runTransaction(postDislike, (post) => {
-      if (post) {
-        if (post.likes && post.likes > 0) {
-          post.likes -= 1;
-        } else {
-          post.likes = 0;
-        }
+  const postDislike = ref(db, `/posts/${postId}`);
+  return runTransaction(postDislike, (post) => {
+    if (post) {
+      if (post.dislikes) {
+        post.dislikes += 1;
+      } else {
+        post.dislikes = 1;
       }
-      return post;
-    });
+    }
+    return post;
+  });
 };
 
 export const addCommentToPost = async (postId, author, comment) => {
-    const postSnapshot = await get(ref(db, `posts/${postId}`));
-    if (!postSnapshot.exists()) {
-      throw new Error('Post not found.');
-    }
-    const post = postSnapshot.val();
-    post.comments.push({ author, comment, commentedOn: Date.now() });
-    return update(ref(db, `posts/${postId}`), post);
-  };
+  const postSnapshot = await get(ref(db, `posts/${postId}`));
+  if (!postSnapshot.exists()) {
+    throw new Error('Post not found.');
+  }
+  const post = postSnapshot.val();
+  post.comments.push({ author, comment, commentedOn: Date.now() });
+  return update(ref(db, `posts/${postId}`), post);
+};
--- client/src/views/Posts/CreatePost.jsx
+++ client/src/views/Posts/CreatePost.jsx
@@ -1,6 +1,46 @@
-import { useContext, useState } from "react";
-import { AppContext } from "../context/AppContext";
+import { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+import { addPost } from '../../services/post.service';
+import Button from '../../components/Button/Button';
 
-export default function CreatePost () {
-    
+export default function CreatePost() {
+  const { userData } = useContext(AppContext);
+  const [post, setPost] = useState({
+    title: '',
+    content: '',
+  });
+
+  const updatePost = (value, key) => {
+    setPost({
+      ...post,
+      [key]: value,
+    });
+  };
+
+  const createPost = async () => {
+    if (post.title.length < 16 || post.title.length > 64) {
+      return alert('Title must be between 16 and 64 symbols.');
+    }
+    if (post.content.length < 32 || post.content.length > 8192) {
+      return alert('Content must be between 32 and 8192 symbols.');
+    }
+
+    await addPost(userData.handle, post.title, post.content);
+
+    setPost({
+      title: '',
+      content: '',
+    });
+  };
+
+  return (
+    <div>
+    <div>New post</div>
+    <label htmlFor="input-title"> Title:</label>
+    <input value={post.title} onChange={e => updatePost(e.target.value, 'title')} type='text' name='input-title' id='input-title'/> <br/>
+    <label htmlFor="input-content"> Content:</label><br/>
+    <textarea value={post.content} onChange={e => updatePost(e.target.value, 'content')} name='input-content' id='input-content' cols='30' rows="10" ></textarea><br/><br/>
+    <Button onClick={createPost}>New Post+</Button>
+    </div>
+  )
 }
