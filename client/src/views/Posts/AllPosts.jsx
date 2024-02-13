79a4618aff4455f252ba89c5835c1c42dc20dcae
borislavpachev <borislav.pachev@gmail.com>
2024-02-13 12:43:35 +0200

    edit button added

--- client/src/views/Posts/AllPosts.jsx
+++ client/src/views/Posts/AllPosts.jsx
@@ -1,21 +1,25 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { getAllPosts } from "../../services/post.service";
 import Post from "../../components/Post/Post";
 import { useSearchParams } from "react-router-dom";
 import { likePost, dislikePost } from '../../services/post.service'; // replace with the actual path
+import { AppContext } from "../../context/AppContext";
 
 export default function AllPosts() {
+  const { user, userData, setAppState } = useContext(AppContext);
   const [posts, setPosts] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
 
   const search = searchParams.get('search') || '';
 
   const setSearch = (value) => {
-    setSearchParams({search: value});
+    setSearchParams({ search: value });
   };
 
   useEffect(() => {
     getAllPosts(search).then(setPosts);
+
+
   }, [search]);
 
   const handleLike = async (id) => {
@@ -28,14 +32,17 @@ export default function AllPosts() {
     setPosts(posts.map(post => post.id === id ? updatedPost : post));
   };
 
+
   return (
     <div>
       <h1>All posts</h1>
-        <label htmlFor="search">Search </label>
-        <input value={search} onChange={e => setSearch(e.target.value)} type="text" name="search" id="search" /><br/>
-        {posts.map((post) => (
-          <Post key={post.id} post={post} onLike={() => handleLike(post.id)} onDislike={() => handleDislike(post.id)}/>
-        ))}
+      <label htmlFor="search">Search </label>
+      <input value={search} onChange={e => setSearch(e.target.value)} type="text" name="search" id="search" /><br />
+      {posts.map((post) => {
+        const hasButton = post.author === userData.username ? true : false;
+       return <Post key={post.id} post={post} hasEditButton={hasButton}
+          onLike={() => handleLike(post.id)} onDislike={() => handleDislike(post.id)} />
+      })}
     </div>
   );
 }
