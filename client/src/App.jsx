c3f8075b626a3735490b48ced9655480c2628880
borislavpachev <borislav.pachev@gmail.com>
2024-02-13 10:38:03 +0200

    logged user authentication component added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { AppContext } from './context/AppContext'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-// import Authenticated from './hoc/Authenticated.jsx'
+import Authenticated from './hoc/Authenticated.jsx'
 import Home from './views/Home/Home'
 import Header from './components/Header/Header'
 import Login from './views/Login/Login'
@@ -14,12 +14,12 @@ import { getUserData } from './services/users.service'
 import ForgotPassword from './components/ForgotPassword/ForgotPassword'
 import UpdateAccount from './views/UpdateProfile/UpdateAccount'
 import Sidebar from './components/Sidebar/Sidebar'
-import  {postCategories} from './constants/postCategories.js'
+import { postCategories } from './constants/postCategories.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import AllPosts from './views/Posts/AllPosts'
 import SinglePost from './views/Posts/SinglePost'
-import CreatePost  from './views/Posts/CreatePost'
+import CreatePost from './views/Posts/CreatePost'
 
 
 function App() {
@@ -37,10 +37,10 @@ function App() {
 
   useEffect(() => {
     if (user) {
+
       getUserData(user.uid)
         .then(snapshot => {
           if (snapshot.exists()) {
-
             setAppState({ user, userData: snapshot.val()[Object.keys(snapshot.val())[0]] });
           }
         })
@@ -58,15 +58,15 @@ function App() {
           <Toaster />
           <Header toggle={toggleSidebar} />
           <Sidebar categories={categories} isOpen={showSidebar} />
-          <div className={showSidebar ? 'main-content': 'sidebar-open'} >
+          <div className={showSidebar ? 'main-content' : 'sidebar-open'} >
             <Routes>
               <Route index element={<Home />} />
               <Route path='/login' element={<Login />} />
               <Route path='/create-account' element={<CreateAccount />} />
-              <Route path='/posts' element={<AllPosts />}/>      
-              <Route path='/posts/:id' element={<SinglePost />} />
-              <Route path='/post-create' element={<CreatePost />}/>
-              <Route path='/user-profile' element={<UpdateAccount />} />
+              <Route path='/posts' element={<Authenticated><AllPosts /></Authenticated>} />
+              <Route path='/posts/:id' element={<Authenticated><SinglePost /></Authenticated>} />
+              <Route path='/post-create' element={<Authenticated><CreatePost /></Authenticated>} />
+              <Route path='/user-profile' element={<Authenticated><UpdateAccount /></Authenticated>} />
               <Route path='/forgot-password' element={<ForgotPassword />} />
 
             </Routes>
