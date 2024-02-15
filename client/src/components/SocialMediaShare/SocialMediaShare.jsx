96ea7faa39720c7160ba4b9e900567064a2611b4
borislavpachev <borislav.pachev@gmail.com>
2024-02-15 23:36:41 +0200

    social media share component

--- client/src/components/SocialMediaShare/SocialMediaShare.jsx
+++ client/src/components/SocialMediaShare/SocialMediaShare.jsx
@@ -1,18 +1,43 @@
-import { useState } from "react";
-import {
-    FacebookShareButton, FacebookMessengerShareButton,
-    LinkedinShareButton, TwitterShareButton
-} from "react-share";
+import { useEffect, useState } from "react";
+import PropTypes from 'prop-types';
 
-export default function socialMediaShare() {
-    const [URL, setURL] = useState('');
+import {
+    FacebookShareButton, FacebookIcon,
+    LinkedinShareButton, LinkedinIcon,
+    TwitterShareButton, XIcon
+} from "react-share";
+import { GoShareAndroid } from "react-icons/go";
+import { getPostById } from "../../services/post.service";
+
+
+
+export default function SocialMediaShare({ id }) {
+    const [currentURL, setCurrentURL] = useState(`${window.location.origin}/posts/${id}`);
+    const [visible, setVisible] = useState(false);
 
     return (
-        <div>
-<FacebookShareButton/>
-<FacebookMessengerShareButton/>
-<LinkedinShareButton/>
-<TwitterShareButton/>
-        </div>
+        <>
+            <GoShareAndroid className="share-button" onClick={() => { setVisible(!visible) }} />
+            {visible && (
+                <div className="share-options">
+                    {/* <div className="share-content"> */}
+                    <p>Share options:  </p>
+                    <FacebookShareButton url={currentURL}>
+                        <FacebookIcon round />
+                    </FacebookShareButton>
+                    <TwitterShareButton url={currentURL}>
+                        <XIcon round />
+                    </TwitterShareButton>
+                    <LinkedinShareButton url={currentURL} >
+                        <LinkedinIcon round />
+                    </LinkedinShareButton>
+                </div>
+            )}
+        </>
     )
+}
+
+
+SocialMediaShare.propTypes = {
+    id: PropTypes.string,
 }
