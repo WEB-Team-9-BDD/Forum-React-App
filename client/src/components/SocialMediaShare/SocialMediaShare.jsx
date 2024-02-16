6edc94f607e550bc51fe4879bc5f9bf4d782fa2f
borislavpachev <borislav.pachev@gmail.com>
2024-02-16 13:37:45 +0200

    share buttons with overlay and component behavior fix

--- client/src/components/SocialMediaShare/SocialMediaShare.jsx
+++ client/src/components/SocialMediaShare/SocialMediaShare.jsx
@@ -8,35 +8,39 @@ import {
 import { GoShareAndroid } from "react-icons/go";
 import './SocialMediaShare.css'
 
-
-
-
 export default function SocialMediaShare({ id }) {
     const [currentURL, setCurrentURL] = useState(`${window.location.origin}/posts/${id}`);
     const [visible, setVisible] = useState(false);
 
+    const toggleShare = () => {
+        setVisible(!visible);
+    }
+
     return (
         <div className="share-container">
-            
+
             {visible && (
-                                
+                <>
                     <div className="share-options">
-                        <p>Share:  </p>
+                        <div className='share-overlay' onClick={toggleShare}></div>
                         <div className="share-buttons">
+                        <p>Share:  </p>
                             <FacebookShareButton url={currentURL}>
-                                <FacebookIcon round />
+                                <FacebookIcon round onClick={toggleShare} />
                             </FacebookShareButton>
                             <TwitterShareButton url={currentURL}>
-                                <XIcon round />
+                                <XIcon round onClick={toggleShare} />
                             </TwitterShareButton>
                             <LinkedinShareButton url={currentURL} >
-                                <LinkedinIcon round />
+                                <LinkedinIcon round onClick={toggleShare} />
                             </LinkedinShareButton>
                         </div>
                     </div>
+                </>
             )}
             <GoShareAndroid className="share-button" onClick={() => { setVisible(!visible) }} />
         </div>
+
     )
 }
 
