4def38ce7a48324ce3f06c10025fbd2add0057f9
borislavpachev <borislav.pachev@gmail.com>
2024-02-17 22:03:05 +0200

    upload profile picture added

--- client/src/services/users.service.js
+++ client/src/services/users.service.js
@@ -1,5 +1,8 @@
-import { get, set, ref, query, equalTo, orderByChild } from 'firebase/database';
-import { db } from '../config/firebase-config';
+import { get, set, ref, query, equalTo, orderByChild, push } from 'firebase/database';
+import { db, storage } from '../config/firebase-config';
+import { ref as sRef } from 'firebase/storage';
+import { getDownloadURL, uploadBytes } from 'firebase/storage';
+import { updateProfile } from 'firebase/auth';
 
 export const getUserByUsername = (username) => {
 
@@ -7,11 +10,11 @@ export const getUserByUsername = (username) => {
 };
 
 export const getUserByEmail = (email) => {
-  
-    return get(ref(db, `users'/${email}`));
+
+  return get(ref(db, `users'/${email}`));
 };
 
-export const createUserUsername = (username, firstName, lastName,email, uid, isAdmin, phoneNumber ) => {
+export const createUser = (username, firstName, lastName, email, uid, isAdmin, phoneNumber) => {
 
   return set(ref(db, `users/${username}`), { username, firstName, lastName, uid, email, createdOn: Date.now(), likedPosts: {}, isAdmin, phoneNumber })
 };
@@ -40,4 +43,15 @@ export const getUserDataByUsername = async (username) => {
     return null;
   }
   return snapshot.val();
+}
+
+export const uploadProfilePicture = async (file, user) => {
+
+  const fileRef = sRef(storage,`images/${user.uid}.png`);
+  await uploadBytes(fileRef, file);
+
+  const uploadedPhotoURL = await getDownloadURL(fileRef);
+  updateProfile(user, { photoURL: uploadedPhotoURL });
+
+  console.log('Suppose to be uploaded');
 }
