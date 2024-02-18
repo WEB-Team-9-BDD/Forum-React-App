47c28c687535b2868a8cb8af679c1f7db02a2ac1
borislavpachev <borislav.pachev@gmail.com>
2024-02-18 21:06:59 +0200

    Upload profile fixed with re render on photo upload

--- client/src/components/ProfilePreview/ProfilePreview.jsx
+++ client/src/components/ProfilePreview/ProfilePreview.jsx
@@ -1,28 +1,34 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import { AppContext } from "../../context/AppContext"
 import PropTypes from 'prop-types';
-import { CgProfile } from "react-icons/cg";
 import './ProfilePreview.css'
-
+import { CgProfile } from "react-icons/cg";
 export default function ProfilePreview({ photoURL, setProfilePhoto, uploadPhoto, userPosts, photo }) {
     const { userData } = useContext(AppContext);
 
+    const handleInputChange = (e) => {
+        if (e.target.files[0]) {
+            setProfilePhoto(e.target.files[0])
+        }
+    }
+//   console.log(photoURL);
     return (
         <div className="profile-preview">
             <h3>Profile preview</h3>
             <div >
-                {(photoURL === '') ?
+                {(!photoURL) ?
                     <CgProfile className="profile-avatar-icon" /> :
                     <img alt="avatar" className='profile-avatar' src={photoURL} />
                 }
                 <div>
                     <input type="file" accept="image/*"
-                        onChange={(e) => setProfilePhoto(e.target.files[0])} />
+                        onChange={handleInputChange} />
                     <p>Username: {userData.username}</p>
                     <p>Registered: {new Date(userData.createdOn).toLocaleDateString('bg-BG')}</p>
                     <p>Number of posts: {userPosts.length}</p>
                     <p>Likes: X</p>
-                    <button className="update-profile-button" disabled={(!photo)}
+                    <button className="update-profile-button"
+                        disabled={(!photo)}
                         onClick={uploadPhoto}>Upload AVATAR</button>
                 </div>
             </div>
