2e8ef2aac57a8b1da781291ae6082e33d9b24db5
borislavpachev <borislav.pachev@gmail.com>
2024-02-19 11:44:30 +0200

    header profile dropdown added

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,21 +1,37 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import "./Header.css"
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../../context/AppContext";
 import { logoutUser } from "../../services/auth.service";
-import { RxHamburgerMenu } from "react-icons/rx";
 import PropTypes from 'prop-types'
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineSettings } from "react-icons/md";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaPowerOff } from "react-icons/fa";
 
 
 export default function Header({ toggle }) {
     const { user, userData, setAppState } = useContext(AppContext);
-
+    const [visible, setVisible] = useState(false);
 
     const navigate = useNavigate();
+
     const logout = async () => {
         await logoutUser();
         setAppState({ user: null, userData: null });
+        toggleProfile();
         navigate('/');
+    }
+
+    const getUserInitials = () => {
+        const firstName = userData.firstName[0].toUpperCase();
+        const lastName = userData.lastName[0].toUpperCase();
+
+        return firstName + lastName;
+    }
+
+    const toggleProfile = () => {
+        setVisible(!visible);
     }
 
     return (
@@ -32,11 +48,28 @@ export default function Header({ toggle }) {
                     ) : (
                         <>
                             {`Welcome, ${userData?.username} `}
-                            <button className="btn btn-primary" onClick={logout}>Log out</button>
                         </>
                     )
                 }
-                {user && <NavLink to="/user-profile">Profile</NavLink>}
+                {user &&
+                    <>
+                        <div className="profile-view" onClick={toggleProfile}>
+                            <img src={`${user.photoURL}`} alt="avatar" className="header-profile-pic" />
+                            <p>{getUserInitials()}</p>
+                        </div>
+                        {visible && (
+                            <div className="header-profile">
+                                <div className="profile-overlay" onClick={toggleProfile}></div>
+                                <div className="header-profile-content">
+                                    <NavLink to="/user-profile" onClick={toggleProfile}><CgProfile className="profile-icons" />Profile</NavLink>
+                                    <NavLink to="/update-profile" onClick={toggleProfile}><MdOutlineSettings className="profile-icons" />Settings</NavLink>
+                                    <div className="logout" onClick={logout}> <FaPowerOff className="profile-icons" />
+                                        Log out</div>
+                                </div>
+                            </div>
+                        )}
+                    </>
+                }
             </header >
         </>
     )
