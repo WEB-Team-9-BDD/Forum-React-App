9306d1973772ea2519963f01dad2687d0159ef91
borislavpachev <borislav.pachev@gmail.com>
2024-02-21 00:49:48 +0200

    category title and style

--- client/src/views/Category/Category.jsx
+++ client/src/views/Category/Category.jsx
@@ -3,31 +3,36 @@ import { useParams } from 'react-router-dom';
 import { getPostsByCategory } from "../../services/post.service";
 import PostPreview from '../../components/PostPreview/PostPreview';
 import './Category.css';
+import { postCategories } from "../../constants/postCategories";
 
-export default function PostsByCategory() {    
-    const { category } = useParams();
-    const [posts, setPosts] = useState([]);
-  
-    useEffect(() => {
-        const fetchPosts = async () => {
-          try {
-            const postsByCategory = await getPostsByCategory(category);
-            setPosts(postsByCategory);
-          } catch (error) {
-            console.error(error); // log any errors
-          }
-        };
-      
-        fetchPosts();
-      }, [category]);
-  
-    return (
-      <div className="category-container">
-        <h1>{category}</h1>
-        {posts.map(post => (
-          <PostPreview key={post.id} post={post} />
-        ))}
+export default function PostsByCategory() {
+  const { category } = useParams();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const postsByCategory = await getPostsByCategory(category);
+        setPosts(postsByCategory);
+      } catch (error) {
+        console.error(error); // log any errors
+      }
+    };
+
+    fetchPosts();
+  }, [category]);
+
+  const categoryName = category.split('-').join(' ').toUpperCase();
+
+  return (
+    <div className="category-container">
+      <div>
+        <h1>{categoryName}</h1>
       </div>
-    );
+      {posts.map(post => (
+        <PostPreview key={post.id} post={post} />
+      ))}
+    </div>
+  );
 }
 
